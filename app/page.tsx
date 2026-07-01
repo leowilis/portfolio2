a01d95885c4613c0f7ed09861bd2a5b1aa60ae22
Leo wilis <leowilis9898@gmail.com>
2026-07-02 00:13:10 +0700

    feat(about): add about section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import AboutSection from '@/src/components/sections/about/AboutSection';
 import HeroSection from '@/src/components/sections/HeroSection';
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
+      <AboutSection />
     </main>
   );
 }
