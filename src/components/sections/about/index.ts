9453c95001cbc3257654ce4243c4148861c80313
Leo wilis <leowilis9898@gmail.com>
2026-07-02 17:26:48 +0700

    feat(about): establish index.ts barrel export for complete encapsulation

---
+++ src/components/sections/about/index.ts
@@ -0,0 +1,17 @@
+export { default as AboutSection } from './AboutSection';
+
+export { default as AboutContent } from './AboutContent';
+
+export { default as AboutHeader } from './AboutHeader';
+
+export { default as AboutBio } from './AboutBio';
+
+export { default as AboutDetails } from './AboutDetails';
+
+export { default as AboutStats } from './AboutStats';
+
+export { default as AboutOrb } from './AboutOrb';
+
+export { default as StatCard } from './StatCard';
+
+export * from './about.data';
