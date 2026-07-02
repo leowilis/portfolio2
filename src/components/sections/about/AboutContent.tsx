592838056e79bcee3c47943818616f85e957177b
Leo wilis <leowilis9898@gmail.com>
2026-07-02 18:04:56 +0700

    refactor(about): streamline AboutContent by wrapping sub-columns in shared FadeIn primitive

--- src/components/sections/about/AboutContent.tsx
+++ src/components/sections/about/AboutContent.tsx
@@ -1,13 +1,18 @@
-'use client';
+import FadeIn from '@/src/animations/FadeIn';
 
 import AboutBio from './AboutBio';
 import AboutDetails from './AboutDetails';
 
 export default function AboutContent() {
   return (
-    <div className='grid items-start gap-16 md:grid-cols-2'>
-      <AboutBio />
-      <AboutDetails />
+    <div className="grid items-start gap-16 md:grid-cols-2">
+      <FadeIn>
+        <AboutBio />
+      </FadeIn>
+
+      <FadeIn y={40}>
+        <AboutDetails />
+      </FadeIn>
     </div>
   );
 }
