67d10a01a9e20f44ea89a9ee2f5df46fbb4dd439
Leo wilis <leowilis9898@gmail.com>
2026-07-02 17:14:45 +0700

    feat(hero): establish index.ts barrel export for complete encapsulation

---
+++ src/components/sections/hero/index.ts
@@ -0,0 +1,19 @@
+export { default as HeroSection } from './HeroSection';
+
+export { default as HeroContent } from './HeroContent';
+
+export { default as HeroHeading } from './HeroHeading';
+
+export { default as HeroDescription } from './HeroDescription';
+
+export { default as HeroButtons } from './HeroButtons';
+
+export { default as HeroTypewriter } from './HeroTypewriter';
+
+export { default as HeroTechStack } from './HeroTechStack';
+
+export { default as HeroAvailability } from './HeroAvailability';
+
+export { default as HeroScrollIndicator } from './HeroScrollIndicator';
+
+export * from './hero.data';
