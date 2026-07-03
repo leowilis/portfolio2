1de09173a9f59a96c2549e90d824db5e2a1369c7
Leo wilis <leowilis9898@gmail.com>
2026-07-03 15:06:26 +0700

    chore(animations): establish master entry index.ts barrel export file

---
+++ src/animations/index.ts
@@ -0,0 +1,7 @@
+export { default as FadeIn } from './FadeIn';
+export { default as ScrollScene } from './ScrollScene';
+export { default as StaggerContainer } from './StaggerContainer';
+export { default as CountUp } from './CountUp';
+export { default as Magnetic } from './Magnetic';
+export { default as Floating } from './Floating';
+export { default as TextReveal } from './TextReveal';
