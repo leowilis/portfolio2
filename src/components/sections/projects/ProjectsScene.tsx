2855aecbc7aeab21d7882fdefa67987c4f1b82e0
Leo wilis <leowilis9898@gmail.com>
2026-07-03 15:24:48 +0700

    feat(projects): upgrade ProjectScene to a layered 3D depth stack layout

--- src/components/sections/projects/ProjectsScene.tsx
+++ src/components/sections/projects/ProjectsScene.tsx
@@ -1,16 +1,45 @@
+'use client';
+
 import { PROJECTS } from './project.data';
 import ProjectCard from './ProjectCard';
 
-export default function ProjectsScene() {
+export default function ProjectScene() {
   return (
     <div
-      className='mt-20 grid gap-12 md:grid-cols-2'
-      style={{
-        perspective: '2000px',
-      }}
+      className='
+        relative
+        mx-auto
+        mt-12
+        flex
+        flex-col
+        gap-8
+        h-auto
+        w-full
+        md:mt-20
+        md:block
+        md:h-[850px]
+        md:perspective-[2200px]
+      '
     >
-      {PROJECTS.map((project) => (
-        <ProjectCard key={project.id} project={project} />
+      {PROJECTS.map((project, index) => (
+        <div
+          key={project.id}
+          className='w-full md:absolute md:left-1/2 md:top-1/2'
+          style={{
+            /* Wrapped in a JS helper window check or standard media utility */
+            transform:
+              typeof window !== 'undefined' && window.innerWidth >= 768
+                ? `
+              translate(-50%, -50%)
+              translateY(${index * 120}px)
+              translateZ(${-index * 180}px)
+              rotateX(10deg)
+            `
+                : undefined,
+          }}
+        >
+          <ProjectCard project={project} />
+        </div>
       ))}
     </div>
   );
