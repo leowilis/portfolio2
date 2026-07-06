6394c3df80e7713661415aeed37a3ed309d1087a
Leo wilis <leowilis9898@gmail.com>
2026-07-06 16:39:36 +0700

    refactor(projects): reorganize project components

--- src/components/sections/projects/ProjectsSection.tsx
+++ src/components/sections/projects/ProjectsSection.tsx
@@ -1,16 +1,47 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
 import { StaggerContainer } from '@/src/animations';
-import ProjectsGrid from './ProjectsGrid';
+
+import MobileProjectCard from './MobileProjectCard';
 import ProjectsHeader from './ProjectsHeader';
 import ProjectsScene from './ProjectsScene';
 
+import { PROJECTS } from './project.data';
+import { MOBILE_BREAKPOINT } from './project.constants';
+
 export default function ProjectsSection() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    function checkViewport() {
+      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
+    }
+    checkViewport();
+    window.addEventListener('resize', checkViewport);
+    return () => {
+      window.removeEventListener('resize', checkViewport);
+    };
+  }, []);
+
+  const mobileProjects = PROJECTS.map((project, index) => (
+    <MobileProjectCard key={project.id} project={project} index={index} />
+  ));
+
   return (
-    <section id='projects' className='relative mx-auto max-w-7xl px-6 py-32'>
+    <section id='projects' className='relative overflow-hidden py-32'>
       <StaggerContainer>
         <ProjectsHeader />
-        <ProjectsGrid />
-        <ProjectsScene />
       </StaggerContainer>
+
+      {isMobile ? (
+        <div className='mx-auto mt-12 max-w-lg flex flex-col gap-6 px-6'>
+          {mobileProjects}
+        </div>
+      ) : (
+        <ProjectsScene />
+      )}
     </section>
   );
 }
