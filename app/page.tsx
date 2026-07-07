87feb2985d667d79e6d46d167cab15032bb8b244
Leo wilis <leowilis9898@gmail.com>
2026-07-07 16:25:16 +0700

    feat(page): mount ProjectsSection inside master ScrollScene layout container

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import ScrollScene from '@/src/animations/ScrollScene';
 import AboutSection from '@/src/components/sections/about/AboutSection';
 import HeroSection from '@/src/components/sections/hero/HeroSection';
+import ProjectsSection from '@/src/components/sections/projects/ProjectsSection';
 
 export default function Home() {
   return (
     <ScrollScene>
       <HeroSection />
       <AboutSection />
+      <ProjectsSection />
     </ScrollScene>
   );
 }
