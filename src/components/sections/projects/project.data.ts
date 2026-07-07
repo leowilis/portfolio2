e640c2add36adfefa7b6bc517fbb10b37d967af0
Leo wilis <leowilis9898@gmail.com>
2026-07-07 16:20:56 +0700

    style(projects): format project.data.ts for better readability and reorder link keys

--- src/components/sections/projects/project.data.ts
+++ src/components/sections/projects/project.data.ts
@@ -1,13 +1,4 @@
-export type Project = {
-  id: number;
-  title: string;
-  description: string;
-  image: string;
-  technologies: string[];
-  github: string;
-  demo: string;
-  featured?: boolean;
-};
+import type { Project } from './project.type';
 
 export const PROJECTS: Project[] = [
   {
@@ -15,10 +6,12 @@ export const PROJECTS: Project[] = [
     title: 'Movie App',
     description:
       'Movie discovery platform with search, detailed information, and a responsive user interface.',
+
     image: '/projects/movie-app.png',
+
     technologies: [
       'React',
-      'React Query',
+      'TanStack Query',
       'React Router',
       'TypeScript',
       'Redux',
@@ -26,8 +19,10 @@ export const PROJECTS: Project[] = [
       'Framer Motion',
       'Sonner',
     ],
-    github: 'https://github.com/leowilis/Movie-app.git',
+
     demo: 'https://movie-app-seven-wheat.vercel.app/',
+    github: 'https://github.com/leowilis/Movie-app.git',
+
     featured: true,
   },
 
@@ -36,6 +31,7 @@ export const PROJECTS: Project[] = [
     title: 'Foody App',
     description:
       'Food ordering platform with authentication, search, favorites, cart management, and a responsive user interface.',
+
     image: '/projects/ecommerce.png',
     technologies: [
       'React',
@@ -44,8 +40,9 @@ export const PROJECTS: Project[] = [
       'Tailwind',
       'TanStack Query',
     ],
-    github: 'https://github.com/leowilis/foody-app.git',
+
     demo: 'https://foody-app-mu.vercel.app/',
+    github: 'https://github.com/leowilis/foody-app.git',
   },
 
   {
@@ -53,6 +50,7 @@ export const PROJECTS: Project[] = [
     title: 'Library App',
     description:
       'Digital library application with book discovery, search functionality, and intuitive navigation.',
+
     image: '/projects/dashboard.png',
     technologies: [
       'Next.js',
@@ -61,7 +59,8 @@ export const PROJECTS: Project[] = [
       'Tailwind',
       'TanStack Query',
     ],
-    github: 'https://github.com/leowilis/library-app.git',
+
     demo: 'https://library-app-indol-nu.vercel.app/',
+    github: 'https://github.com/leowilis/library-app.git',
   },
 ];
