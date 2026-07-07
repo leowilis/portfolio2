0b4a452709a709f6af0f1268e55bbe526a0308bb
Leo wilis <leowilis9898@gmail.com>
2026-07-07 16:23:36 +0700

    chore(projects): remove unused project.utils.ts

--- src/components/sections/projects/project.utils.ts
+++
@@ -1,9 +0,0 @@
-import type { Project } from './project.type';
-
-export function getProjectUrl(project: Project) {
-  if (project.demo !== '#') {
-    return project.demo.replace('https://', '');
-  }
-
-  return `${project.title.toLowerCase().replace(/\s+/g, '-')}.vercel.app`;
-}
