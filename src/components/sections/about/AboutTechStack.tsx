a5cf6766ea9aba48417480a61e146a4c90b8ddd8
Leo wilis <leowilis9898@gmail.com>
2026-07-27 23:52:00 +0700

    refactor: improve AboutTechStack semantics

--- src/components/sections/about/AboutTechStack.tsx
+++ src/components/sections/about/AboutTechStack.tsx
@@ -2,29 +2,20 @@ import { TECHS } from './about.data';
 
 export default function AboutTechStack() {
   return (
-    <div className='flex flex-wrap gap-2 pt-2'>
-      {TECHS.map((tech) => (
-        <span
-          key={tech}
-          className='
-            rounded-full
-            border
-            border-white/10
-            bg-white/5
-            px-3
-            py-1
-            text-[11px]
-            text-white/30
-            transition-all
-            duration-300
-            hover:border-violet-500/40
-            hover:bg-violet-500/10
-            hover:text-violet-300
-          '
-        >
-          {tech}
-        </span>
-      ))}
-    </div>
+    <section aria-labelledby='tech-stack-heading' className='pt-2'>
+      <h3 id='tech-stack-heading' className='sr-only'>
+        Technology Stack
+      </h3>
+
+      <ul className='flex flex-wrap gap-2'>
+        {TECHS.map((tech) => (
+          <li key={tech} className='list-none'>
+            <span className='rounded-full border border-white/10 bg-white/5 px-3 py-1 text-[11px] text-white/30 transition-all duration-300 hover:border-violet-500/40 hover:bg-violet-500/10 hover:text-violet-300'>
+              {tech}
+            </span>
+          </li>
+        ))}
+      </ul>
+    </section>
   );
 }
