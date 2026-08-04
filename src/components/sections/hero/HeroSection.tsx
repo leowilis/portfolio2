85192b83cf126a849426c9f544a226be04bcaf95
Leo wilis <leowilis9898@gmail.com>
2026-08-04 18:44:50 +0700

    refactor(hero): refine section spacing

--- src/components/sections/hero/HeroSection.tsx
+++ src/components/sections/hero/HeroSection.tsx
@@ -1,15 +1,19 @@
 import HeroAvailability from './HeroAvailability';
 import HeroContent from './HeroContent';
 import HeroScrollIndicator from './HeroScrollIndicator';
-import HeroTechStack from './HeroTechStack';
 
 export default function HeroSection() {
   return (
-    <section className='relative flex min-h-screen flex-col items-center justify-center overflow-hidden px-6'>
+    <section className='relative flex min-h-screen flex-col items-center justify-center overflow-hidden px-6 pt-16 pb-10 lg:pt-24'>
       <HeroContent />
-      <HeroTechStack />
-      <HeroAvailability />
-      <HeroScrollIndicator />
+
+      <div className='mt-12'>
+        <HeroAvailability />
+      </div>
+
+      <div className='mt-16'>
+        <HeroScrollIndicator />
+      </div>
     </section>
   );
 }
