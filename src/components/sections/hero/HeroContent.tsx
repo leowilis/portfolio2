059d8a83ae2045d2052959b3795c0cae0918cf84
Leo wilis <leowilis9898@gmail.com>
2026-08-05 16:36:19 +0700

    refactor(hero): clean up content composition

--- src/components/sections/hero/HeroContent.tsx
+++ src/components/sections/hero/HeroContent.tsx
@@ -6,7 +6,7 @@ import HeroTypewriter from './HeroTypewriter';
 
 export default function HeroContent() {
   return (
-    <div className='flex w-full max-w-5xl -translate-y-4 flex-col items-center text-center lg:-translate-y-8 lg:scale-[1.06]'>
+    <div className='flex w-full max-w-5xl -translate-y-4 flex-col items-center text-center lg:-translate-y-12 lg:scale-[1.06]'>
       <HeroHeading />
 
       <HeroTypewriter />
