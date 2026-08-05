90120301ad84b502524430d17b27316d0d133a28
Leo wilis <leowilis9898@gmail.com>
2026-08-05 16:36:55 +0700

    refactor(hero): clean up section composition

--- src/components/sections/hero/HeroSection.tsx
+++ src/components/sections/hero/HeroSection.tsx
@@ -1,19 +1,21 @@
+import HeroAmbientGlow from './HeroAmbientGlow';
 import HeroAvailability from './HeroAvailability';
 import HeroContent from './HeroContent';
+import HeroMouseGlow from './HeroMouseGlow';
 import HeroScrollIndicator from './HeroScrollIndicator';
 
 export default function HeroSection() {
   return (
     <section className='relative flex min-h-screen flex-col items-center justify-center overflow-hidden px-6 pt-16 pb-10 lg:pt-24'>
+      <HeroAmbientGlow />
+
+      <HeroMouseGlow />
+
       <HeroContent />
 
-      <div className='mt-12'>
-        <HeroAvailability />
-      </div>
+      <HeroAvailability />
 
-      <div className='mt-16'>
-        <HeroScrollIndicator />
-      </div>
+      <HeroScrollIndicator />
     </section>
   );
 }
