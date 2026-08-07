d5c2f8b5c74e7ed80edd8d351f66b57f9a60cd72
Leo wilis <leowilis9898@gmail.com>
2026-08-07 17:32:33 +0700

    refactor(projects): improve responsive projects section

--- src/components/sections/projects/ProjectsSection.tsx
+++ src/components/sections/projects/ProjectsSection.tsx
@@ -1,43 +1,61 @@
 'use client';
 
-import { useEffect, useState } from 'react';
-
+import { useSyncExternalStore } from 'react';
 import { StaggerContainer } from '@/src/animations';
-
 import MobileProjectCard from './MobileProjectCard';
 import ProjectsHeader from './ProjectsHeader';
 import ProjectsScene from './ProjectsScene';
-
-import { PROJECTS } from './project.data';
 import { MOBILE_BREAKPOINT } from './project.constants';
+import { PROJECTS } from './project.data';
+
+const MOBILE_QUERY = `(max-width: ${MOBILE_BREAKPOINT - 1}px)`;
+
+function subscribeToMobileQuery(callback: () => void) {
+  const mediaQuery = window.matchMedia(MOBILE_QUERY);
+  mediaQuery.addEventListener('change', callback);
+  return () => {
+    mediaQuery.removeEventListener('change', callback);
+  };
+}
+
+function getMobileSnapshot() {
+  return window.matchMedia(MOBILE_QUERY).matches;
+}
+
+function getMobileServerSnapshot() {
+  return false;
+}
+
+function useIsMobile() {
+  return useSyncExternalStore(
+    subscribeToMobileQuery,
+    getMobileSnapshot,
+    getMobileServerSnapshot,
+  );
+}
 
 export default function ProjectsSection() {
-  const [isMobile, setIsMobile] = useState(false);
-
-  useEffect(() => {
-    function checkViewport() {
-      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
-    }
-    checkViewport();
-    window.addEventListener('resize', checkViewport);
-    return () => {
-      window.removeEventListener('resize', checkViewport);
-    };
-  }, []);
-
-  const mobileProjects = PROJECTS.map((project, index) => (
-    <MobileProjectCard key={project.id} project={project} index={index} />
-  ));
+  const isMobile = useIsMobile();
 
   return (
-    <section id='projects' className='relative overflow-hidden'>
+    <section
+      id='projects'
+      aria-labelledby='projects-heading'
+      className='relative overflow-hidden py-16 sm:py-24'
+    >
       <StaggerContainer>
         <ProjectsHeader />
       </StaggerContainer>
 
       {isMobile ? (
-        <div className='mx-auto mt-12 max-w-lg flex flex-col gap-6 px-6'>
-          {mobileProjects}
+        <div className='mx-auto mt-12 flex max-w-lg flex-col gap-6 px-6'>
+          {PROJECTS.map((project, index) => (
+            <MobileProjectCard
+              key={project.id}
+              project={project}
+              index={index}
+            />
+          ))}
         </div>
       ) : (
         <ProjectsScene />
