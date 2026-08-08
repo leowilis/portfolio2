f8d33e21ddd756f72652d27e99ff99e3a6b373cc
Leo wilis <leowilis9898@gmail.com>
2026-08-08 17:55:59 +0700

    feat: add scroll restoration components

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import ClientProviders from '@/src/components/layout/ClientProviders';
 import MainNavbar from '@/src/components/layout/Navbar';
+import ScrollRestorationScript from '@/src/components/ScrollRestoration/ScrollRestorationScript';
+import ScrollRestoration from '@/src/components/ScrollRestoration/ScrollRestoration';
 
 const geistSans = Geist({ variable: '--font-geist-sans', subsets: ['latin'] });
 const geistMono = Geist_Mono({
@@ -29,6 +31,8 @@ export default function RootLayout({
       <body className='min-h-full flex flex-col'>
         <ClientProviders>
           <MainNavbar />
+          <ScrollRestorationScript />
+          <ScrollRestoration />
           {children}
         </ClientProviders>
       </body>
