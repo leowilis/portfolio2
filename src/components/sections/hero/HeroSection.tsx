5e6de82d4867672271d094cc76c126afbd75b691
Leo wilis <leowilis9898@gmail.com>
2026-08-10 18:40:41 +0700

    refactor: update svh in hero section

--- src/components/sections/hero/HeroSection.tsx
+++ src/components/sections/hero/HeroSection.tsx
@@ -6,7 +6,7 @@ import HeroScrollIndicator from './HeroScrollIndicator';
 
 export default function HeroSection() {
   return (
-    <section className='relative flex min-h-[100svh] flex-col items-center justify-center overflow-hidden px-5 py-24 sm:px-6 sm:py-28 lg:py-20'>
+    <section className='relative flex min-h-[49svh] flex-col items-center justify-center overflow-hidden px-5 py-24 sm:px-6 sm:py-28 lg:py-20'>
       <HeroAmbientGlow />
 
       <HeroMouseGlow />
