bf52ed61e961e0b2dde5f363d303f42a88b1ee92
Leo wilis <leowilis9898@gmail.com>
2026-08-12 23:28:27 +0700

    refactor: move to sections folder

--- src/components/education/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as EducationSection } from './EducationSection';
-export { default as EducationHeader } from './EducationHeader';
-export { default as EducationCard } from './EducationCard';
