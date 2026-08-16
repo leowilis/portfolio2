5cd908a8a2e65b680fd1ae94b819bb2254423bc9
Leo wilis <leowilis9898@gmail.com>
2026-08-16 15:13:12 +0700

    feat(page): assemble master landing sections layout with education and contact

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,22 @@
 import ScrollScene from '@/src/animations/ScrollScene';
 import AboutSection from '@/src/components/sections/about/AboutSection';
+import ContactSection from '@/src/components/sections/contact/ContactSection';
+import EducationSection from '@/src/components/sections/education/EducationSection';
 import HeroSection from '@/src/components/sections/hero/HeroSection';
 import ProjectsSection from '@/src/components/sections/projects/ProjectsSection';
 import TechStack from '@/src/components/sections/tech-stack';
 
 export default function Home() {
   return (
-    <ScrollScene>
-      <HeroSection />
-      <AboutSection />
-      <ProjectsSection />
-      <TechStack />
-    </ScrollScene>
+    <main className='w-full relative'>
+      <ScrollScene>
+        <HeroSection />
+        <AboutSection />
+        <ProjectsSection />
+        <TechStack />
+        <EducationSection />
+        <ContactSection />
+      </ScrollScene>
+    </main>
   );
 }
